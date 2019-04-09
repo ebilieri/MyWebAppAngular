ddbc7755ba87c0bc4b386d96b24c026c4e1cf3da
Emerson Bilieri Cluadelino <ebilieri@hotmail.com>
2019-04-08 21:18:08 -0300

    angula

---
+++ MyWebApp/dist/out-tsc/wwwroot/ts/principal.js
@@ -0,0 +1,4 @@
+import { Produto } from "./produto";
+var produto = new Produto("sigla-teste", 10);
+produto.exibirSigla();
+//# sourceMappingURL=principal.js.map
---
+++ MyWebApp/dist/out-tsc/wwwroot/ts/produto.js
@@ -0,0 +1,25 @@
+var Produto = /** @class */ (function () {
+    function Produto(sigla, preco) {
+        this.sigla = sigla;
+        this.preco = preco;
+        this.id = 0;
+    }
+    Produto.prototype.exibirSigla = function () {
+        alert(this.sigla);
+    };
+    Produto.prototype.obterNome = function () {
+        return this.nome;
+    };
+    Object.defineProperty(Produto.prototype, "descricao", {
+        get: function () {
+            return this.nome;
+        },
+        set: function (val) {
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Produto;
+}());
+export { Produto };
+//# sourceMappingURL=produto.js.map
